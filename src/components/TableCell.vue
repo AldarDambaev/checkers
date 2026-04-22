7ff7bd1346462bf29a285d0d1169db9bbf6ea5c7
AldarDambaev <repeater.dv@gmail.com>
2026-04-22 20:35:04 +0300

    1

---
+++ src/components/TableCell.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="table-cell">
+    <div
+      v-if="figureType !== 0"
+      class="table-cell__figure"
+      :class="{
+        'table-cell__figure--white': figureType === 1,
+        'table-cell__figure--black': figureType === 2
+      }"
+    />
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+
+  const props = defineProps({
+    data: {
+      type: Object,
+      default: () => ({}),
+    },
+  });
+
+  const figureType = ref(props.data.figureType);
+  
+  function onTable(way) {
+    const { cx, cy } = way;
+
+    return cx >= 0 && cx < 8 && cy >= 0 && cy < 8;
+  }
+
+const emit = defineEmits(["showWay"]);
+
+  function findWays() {
+    let ways = [
+      {
+        position: "topLeft",
+        cx: cx.value - 1,
+        cy: cy.value - 1,
+      },
+      {
+        position: "topRight",
+        cx: cx.value + 1,
+        cy: cy.value - 1,
+      },
+      {
+        position: "bottomLeft",
+        cx: cx.value - 1,
+        cy: cy.value + 1,
+      },
+      {
+        position: "bottomRight",
+        cx: cx.value + 1,
+        cy: cy.value + 1,
+      },
+    ].filter((item) => onTable(item));
+
+    emit("showWay", { ways });
+  }
+</script>
+
+<style lang="less">
+  .table-cell {
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    height: 100px;
+
+    &__figure {
+      box-sizing: border-box;
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      border: 3px solid @gray;
+      cursor: pointer;
+
+      &--black {
+        background-color: @black;
+      }
+
+      &--white {
+        background-color: @white;
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./styles/styles.less"
 
-createApp(App).mount('#app')
+
+import { createApp } from 'vue';
+import { createPinia } from "pinia";
+import App from './App.vue';
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.mount('#app');
